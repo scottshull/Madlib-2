ab272baf9aa5d9990c8dc999adf495fa7ba46893
scottshull <sshull@students.mchenry.edu>
2016-09-12 17:41:15 -0500

    Add files via upload

---
+++ Madlib 2.py
@@ -0,0 +1,97 @@
+#Variable
+
+print("Enter any verb ")
+verb = raw_input()
+if verb == "":
+    verb = "crunch"
+
+print("Enter any object ")
+object = raw_input()
+if object == "":
+    object = "cheeto"
+
+print("Enter any noun ")
+noun = raw_input()
+if noun == "":
+    noun = "pool"
+
+print("Enter any object ")
+object2 = raw_input()
+if object2 == "":
+    object2 = "bat"
+
+print("Enter any adjective ")
+adjective = raw_input()
+if adjective == "":
+    adjective = "hungry"
+
+print("Enter any noun ")
+noun2 = raw_input()
+if noun2 == "":
+    noun2 = "dog"
+
+print("Enter any verb or adverb ")
+verb2 = raw_input()
+if verb2 == "":
+    verb2 = "asleep"
+
+print("Enter any pronoun ")
+pronoun = raw_input()
+if pronoun == "":
+    pronoun = "her"
+
+print("Enter any adjective ")
+adjective2 = raw_input()
+if adjective2 == "":
+    adjective2 = "big"
+
+print("Enter any noun ")
+noun3 = raw_input()
+if noun3 == "":
+    noun3 = "shoe"
+
+print("Enter any noun ")
+noun4 = raw_input()
+if noun3 == "":
+    noun3 = "man"
+
+print("Enter any adjective ")
+adjective3 = raw_input()
+if adjective3 == "":
+    adjective3 = "tasty"
+
+print("Enter any color ")
+color = raw_input()
+if color == "":
+    color = "green"
+
+print("Enter any verb ")
+verb3 = raw_input()
+if verb3 == "":
+    verb3 = "yell"
+
+#poem
+print(verb + ", " + verb + "," + " little " + object + ",")
+print("How I wonder where you are" + "!")
+print("Up above the " + noun + " so high" + ",")
+print("Like a " + object2 + " in the sky" + ",\n")
+
+print("When the " + adjective + " " + noun2  +" is gone" + ",")
+print("When the nothing shines upon" + ",")
+print("Then you show " + pronoun +  " " + adjective2 + " " + noun3 + ",")
+print(verb + ", " + verb + "," + " all night" + ",\n")
+
+print("Then the " + noun4 + "in the dark")
+print("Thanks you for your " + adjective3 + " sparks" ";")
+print("He could not see which way to go " + ",")
+print("If you did not " + verb + " so" ".\n")
+
+print("In the dark " + color + " sky,")
+print("And often through my curtains " + verb3 + ",")
+print("For you never shut your eye")
+print("Till the sun is in the sky" ".\n")
+
+print("As bright as your " + adjective2 + " spark")
+print("Lights the " + noun4 + " in the dark,")
+print("Though I know what you are,")
+print(verb + ", " + verb + "," + " little " + object + ".")
